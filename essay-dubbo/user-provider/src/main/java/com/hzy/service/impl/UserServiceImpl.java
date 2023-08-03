5b35d059fa8e95d4f488537ad97e7983acdb82fc
hello@163.com <[he18270675493@163.com]>
2023-08-03 14:56:45 +0800

    Dubbo简单使用

--- essay-dubbo/user-provider/src/main/java/com/hzy/service/impl/UserServiceImpl.java
+++ essay-dubbo/user-provider/src/main/java/com/hzy/service/impl/UserServiceImpl.java
@@ -2,13 +2,21 @@ package com.hzy.service.impl;
 
 import com.hzy.pojo.User;
 import com.hzy.service.UserService;
+import org.apache.dubbo.config.annotation.Method;
 import org.apache.dubbo.config.annotation.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Override
     public String getUserName(Integer id) {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "getUserName";
     }
 
